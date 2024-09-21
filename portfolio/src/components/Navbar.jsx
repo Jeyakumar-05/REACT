228320713bfd7597f0111df3393142174e94536c
Jeyakumar <jeyakumar1442@gmail.com>
2024-09-21 22:40:02 +0530

    Update name using state (useEffect() used)

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { CircleUser, CircleX } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 import { useRef } from 'react';
 import '../assets/css/style.css'
+import { useEffect } from 'react';
 
 const Navbar = () => {
   
@@ -19,9 +20,15 @@ const Navbar = () => {
      path:"/contact"
     }]
 //now using useRef
-    const emailref = useRef(null)
-    const passwordref = useRef(null)
+    const nameref = useRef(null)
+    const [name, setName] = useState('');
 
+    useEffect(() => {
+        const storedName = localStorage.getItem('name');
+        if(storedName){
+            setName(storedName);
+        }
+    }, [] );
 
 // commented lines are get input using only state
     // const[userdata, setuserdata] = useState({
@@ -32,20 +39,18 @@ const Navbar = () => {
     // const handleChange = (e) => {
     //   setuserdata({ ...userdata, [e.target.id]: e.target.value})
     // }
-
     const handleLogin = (e) => {
       e.preventDefault();
-    //   console.log(userdata);
-    //   console.log("using state");
-         console.log(emailref.current.value, passwordref.current.value)  
+      const newnameValue = nameref.current.value;
+      setName(newnameValue);
+      localStorage.setItem('name', newnameValue) 
     }
   const [visible, setvisible] = useState(false);
   return (
     <>
-
 <div className="h-[4rem] w-full flex justify-center items-center rounded-md bg-white border border-zinc-500">
     <div className="h-full w-[90%] flex flex-row items-center">
-        <div className='w-[50%] h-full flex justify-start items-center font-semibold pinktext text-2xl'><h2>JEYAKUMAR S</h2></div>
+        <div className='w-[50%] h-full flex justify-start items-center font-semibold pinktext text-2xl'><h2>{name ? name:"JEYAKUMAR S"}</h2></div>
         <div className='w-[45%] h-full flex justify-end items-center'>
             <ul className='h-full w-full list-none flex  items-center gap-10 justify-end'>
                {navdata.map((data, index) => (
@@ -63,27 +68,25 @@ const Navbar = () => {
   visible && (
     <>
                         <div className="h-screen w-screen absolute top-0 left-0 bg-black/20 flex justify-center items-center z-50">
-                            <div className=" h-[40%] w-[30%] bg-red z-50 flex flex-col  bg-white  shadow-lg ">
-                                <div className="w-full h-[15%] flex flex-row justify-start px-10 items-center border-2 text-white bg-purple-500 text-xl font-bold ">
+                            <div className=" h-[35%] w-[30%] bg-red z-50 flex flex-col  bg-white shadow-lg ">
+                                <div className="w-full h-[15%] flex flex-row justify-start px-10 items-center border-2 rounded-sm text-gray-600 text-xl font-bold ">
                                     <div className="w-1/2">
-                                        Login
+                                        Profile
                                     </div>
                                     <div className="w-1/2 flex justify-end">
                                         <CircleX onClick={() => setvisible(!visible)} />
                                     </div>
                                 </div>
-                                <div className="w-full h-[85%] flex flex-row justify-center items-center">
-                                    <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" onSubmit={handleLogin}>
+                                <div className="w-full h-[68%] flex flex-row justify-center items-center">
+                                    <form className="w-[80%]  flex flex-col justify-center items-center gap-4" onSubmit={handleLogin}>
                                         {/* <input type="email" name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} />
                                         <input type="password" name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} /> */}
-                                        <input type="email" ref={emailref} name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required/>
-                                        <input type="password" ref={passwordref} name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
-                                        
-                                        <button type="submit" className="bg-green-500 p-3 text-white w-full rounded-sm text-lg font-bold">   Login</button>
+                                        <input type="text" ref={nameref} name="" id="name" placeholder="Name" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-gray-500" required/>          
+                                        <button type="submit" className="bg-amber-400 p-2  text-white w-full rounded-sm text-lg font-bold"> Update Name</button>
                                     </form>
                                 </div>
-                                <div className="w-full h-[15%] flex flex-row justify-center items-center">
-                                    <button onClick={() => setvisible(!visible)} className="bg-red-500 p-3 text-white w-full h-full rounded-sm text-lg font-bold"> Cancel </button>
+                                <div className="w-full h-[18%] flex flex-row  justify-center items-end">
+                                    <button onClick={() => setvisible(!visible)} className="bg-gray-600 p-3 text-white w-full h-full rounded-sm text-lg font-bold"> Cancel </button>
                                 </div>
                             </div>
                         </div>
